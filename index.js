563c706bcc38a8c72faa25a6928292056f664e35
Neilson <Brennan.Neilson@henryschein.com>
2020-01-23 14:34:01 -0700

    First post endpoint is working

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code away!
+const express = require('express');
+const postRoutes = require('./posts/postRouter.js');
+const userRoutes = require('./users/userRouter.js');
+
+function logger(req, res, next) {
+  const now = new Date().toISOString();
+  console.log(`${now}: ${req.method} to ${req.url} from ${req.get('Origin')}`);
+
+  next();
+}
+
+const port = 4000;
+const server = express();
+
+server.use(express.json());
+server.use(logger);
+
+
+server.use('/posts', postRoutes);
+server.use('/users', userRoutes);
+
+server.use(function(req, res, next) {
+  res.status(404).send("That request cannot be processed");
+});
+
+server.listen(port, () => console.log("Listening for changes on port " + port));
--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const db = require('./postDb.js');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
+  db.get().then(response => {
+    res.status(200).json(response);
+  }).catch(err => {
+    res.status(400).json({message: "Could not load posts"})
+  })
 });
 
 router.get('/:id', (req, res) => {
@@ -21,7 +26,11 @@ router.put('/:id', (req, res) => {
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  if (db.getById(req.id)) {
+    next();
+  } else {
+    res.status(404).send({message: "A post with that ID could not be found"});
+  }
 }
 
 module.exports = router;
