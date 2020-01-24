52f8dca8bc737f81f8513fc4fc1bfcaff5b30162
Brennan Neilson <bvneilson@gmail.com>
2020-01-23 21:22:16 -0700

    maybe

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -3,4 +3,4 @@ const knex = require('knex');
 const environment = 'development'
 const knexConfig = require('../knexfile.js')[environment];
 
-module.exports = knex(knexConfig);
+module.exports = knex(knexConfig.development);
